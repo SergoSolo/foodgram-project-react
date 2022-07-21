a8f52d1a57cefb35537eb10aebd0c541fc3ba91c
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-07-21 22:57:47 +0300

    fix docker-compose and admin

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -74,6 +74,7 @@ DATABASES = {
     }
 } 
 
+
 REST_FRAMEWORK = { 
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -3,8 +3,15 @@ from django.contrib import admin
 from .models import Cart, Favorite, Ingredient, IngredientAmount, Recipe, Tag
 
 
+class IngredientInline(admin.TabularInline):
+    model = IngredientAmount
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    inlines = [
+        IngredientInline,
+    ]
     list_display = ('name', 'author', 'count_favorites')
     list_filter = ('name', 'author', 'tags')
 
@@ -19,9 +26,10 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name', 'measurement_unit')
     list_filter = ('name',)
-
+    search_fields = ('name',)
+    empty_value_display = 'пусто'
 
 @admin.register(IngredientAmount)
 class IngredientAmountAdmin(admin.ModelAdmin):

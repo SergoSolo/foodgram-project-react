7b66c26abd727cd67b15b7e09ac4ce37f61ca277
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-07-24 13:18:45 +0300

    fix filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,14 +1,16 @@
+from django_filters import AllValuesMultipleFilter
 from django_filters import rest_framework as filters
 
 from recipes.models import Recipe, Ingredient  # isort:skip
 
 
 class RecipeFilters(filters.FilterSet):
-    author = filters.AllValuesFilter(method='filter_author')
-    tags = filters.AllValuesFilter(
-        field_name='tags__slug',
-        method='filter_tags'
-    )
+    # author = filters.AllValuesFilter(method='filter_author')
+    # tags = filters.AllValuesFilter(
+    #     field_name='tags__slug',
+    #     method='filter_tags'
+    # )
+    tafs = AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
@@ -18,16 +20,16 @@ class RecipeFilters(filters.FilterSet):
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
 
-    def filter_author(self, queryset, name, value):
-        if value:
-            return queryset.filter(author=value)
-        return queryset
+    # def filter_author(self, queryset, name, value):
+    #     if value:
+    #         return queryset.filter(author=value)
+    #     return queryset
 
-    def filter_tags(self, queryset, name, value):
-        if value:
-            return queryset.filter(
-                tags__slug__in=value).distinct()
-        return queryset
+    # def filter_tags(self, queryset, name, value):
+    #     if value:
+    #         return queryset.filter(
+    #             tags__slug__in=value).distinct()
+    #     return queryset
 
     def filter_is_favorited(self, queryset, name, value):
         if value:

a967315ecbff15a89be19bd95749fb2033505085
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-07-24 10:58:00 +0300

    fix 10

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import IngredientViewSet, RecipeViewSet, TagViewSet, UserViewSet
+from .views import (FollowViewSet, IngredientViewSet, RecipeViewSet,
+                    TagViewSet, UserViewSet)
 
 router = SimpleRouter()
 router.register(r'recipes', RecipeViewSet, basename='recipe')
 router.register(r'ingredients', IngredientViewSet, basename='ingredient')
 router.register(r'tags', TagViewSet, basename='tag')
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'users/subscriptions', FollowViewSet, basename='subs')
 
 urlpatterns = [
     path('', include(router.urls)),

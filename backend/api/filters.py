8ea76df71eb6770bfce83b0a7cc81ac5e5e4e2fa
Сергей Соловьев <ser5olovjew@yandex.ru>
2022-07-24 14:14:18 +0300

    fix perm

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,19 +1,9 @@
-# from django_filters import AllValuesMultipleFilter
-# from django_filters import rest_framework as filters
-# from django_filters import FilterSet, filters
 from django_filters.rest_framework import FilterSet, filters
-
-# import django_filters as filters
 
 from recipes.models import Recipe, Ingredient  # isort:skip
 
 
 class RecipeFilters(FilterSet):
-    # author = filters.AllValuesFilter(method='filter_author')
-    # tags = filters.AllValuesFilter(
-    #     field_name='tags__slug',
-    #     method='filter_tags'
-    # )
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
@@ -23,17 +13,6 @@ class RecipeFilters(FilterSet):
     class Meta:
         model = Recipe
         fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
-
-    # def filter_author(self, queryset, name, value):
-    #     if value:
-    #         return queryset.filter(author=value)
-    #     return queryset
-
-    # def filter_tags(self, queryset, name, value):
-    #     if value:
-    #         return queryset.filter(
-    #             tags__slug__in=value).distinct()
-    #     return queryset
 
     def filter_is_favorited(self, queryset, name, value):
         if value:
